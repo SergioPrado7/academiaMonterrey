508ac5bdb3eaa6126d1f509aed8b336f2fc4b1b7
SergioPrado7 <pradosergioslcs275@gmail.com>
2026-08-13 18:26:33 -0600

    Aqui se crearon los objetos como Peliculas y Salas disponibles donde uno puede ver la pelicula

--- Semana1/CineSanPedroCoahuila/src/cinesanpedrocoahuila_v0/Principal.java
+++ Semana1/CineSanPedroCoahuila/src/cinesanpedrocoahuila_v0/Principal.java
@@ -3,7 +3,17 @@ package cinesanpedrocoahuila_v0;
 public class Principal {
 
 	public static void main(String[] args) {
+		Cine cine1 = new Cine();
+		System.out.println("Bienvenidos al " + cine1.getNombreCine());
+		System.out.println("El cual esta ubicado en " + cine1.getUbicacionCine());
 		
+		Pelicula peli1 = new Pelicula("El Padrino", 95);
+		Pelicula peli2 = new Pelicula("Avenger End Game", 115);
+		Pelicula peli3 = new Pelicula("Spiderman Brands New Day", 125);
+		
+		Sala sala1 = new Sala(1, peli1);
+		Sala sala2 = new Sala(2, peli2);
+		Sala sala3 = new Sala(3, peli3);
 	}
 
 }
